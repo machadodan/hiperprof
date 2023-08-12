38b01ed309e54530d7e997f79f6f59ec4ea37f02
machadodan <danielg_m@hotmail.com>
2023-08-12 16:17:05 -0300

    Configurando ApiService com axios

---
+++ src/data/services/ApiServices.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+
+export const ApiService = axios.create({
+    baseURL: "https://alunos.treinaweb.com.br/hyperprof",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept-Language": "pt-BR",
+    },
+});
