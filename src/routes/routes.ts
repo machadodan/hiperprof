e77fc15d8f5a99dce6d4f520f803414262b4e47c
machadodan <danielg_m@hotmail.com>
2023-08-12 16:19:57 -0300

    Configurando roteamento para a tela detalhe do professor

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -16,4 +16,11 @@ export const Router = {
     },
     icon: "",
   },
+  detalheProfessor: {
+    name: "/professor/detalhe-professor",
+    push: function (router: NextRouter) {
+      router.push({ pathname: this.name });
+    },
+    icon: "",
+  },
 };
