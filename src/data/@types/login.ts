810fb9e3e11ee3060deab1d65676a0d5fb441439
machadodan <danielg_m@hotmail.com>
2023-08-21 18:59:56 -0300

    Criando interface de login

---
+++ src/data/@types/login.ts
@@ -0,0 +1,13 @@
+export interface LoginInterface {
+    email: string,
+    password: string,
+}
+
+export interface LoginErroInterface extends LoginInterface {
+
+}
+
+export interface ResponseLoginInterface {
+    refresh_token: string;
+    token: string;
+}
