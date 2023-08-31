fa032d15ae596f92c38386f89d5e80028e579b4b
machadodan <danielg_m@hotmail.com>
2023-08-31 18:52:13 -0300

    criando serviço de interceptando request

--- src/data/services/ApiServices.ts
+++ src/data/services/ApiServices.ts
@@ -1,10 +1,48 @@
-import axios from "axios";
-
+import axios, { AxiosError } from "axios";
 
 export const ApiService = axios.create({
-    baseURL: "https://alunos.treinaweb.com.br/hyperprof",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept-Language": "pt-BR",
-    },
+  baseURL: "https://alunos.treinaweb.com.br/hyperprof",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept-Language": "pt-BR",
+  },
 });
+
+ApiService.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (erro: AxiosError) => {
+    if (erro.request.status === 401) {
+      return await handleTokenRefresh(erro);
+    }
+
+    return Promise.reject(erro);
+  }
+);
+
+async function handleTokenRefresh(erro: AxiosError) {
+  try {
+    const refreshToken = localStorage.getItem("refresh_token_hiperprof");
+    await ApiService.post<{ token: string; refresh_token: string }>(
+      "/api/auth/refresh",
+      {
+        refresh_token: refreshToken,
+      }
+    ).then(({ data }) => {
+      localStorage.setItem("token_hiperProf", data.token);
+      localStorage.setItem("refresh_token_hiperprof", data.refresh_token);
+    });
+    
+    return await ApiService({
+        ...erro.config,
+        headers:{
+            ...erro.config?.headers,
+            Authorization: `Bearer ${localStorage.getItem("token_hiperProf")}`,
+        },
+    })
+
+  } catch (error) {
+    return Promise.reject(erro);
+  }
+}
